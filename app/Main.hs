7509aa4440161985350b77bdfa6531e4a8411654
Alex <alexeusgr@gmail.com>
2023-12-04 16:27:38 +0200

    get the result

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,5 @@ import qualified MyLib (getCalibrationValues)
 main :: IO ()
 main = do
   dt <- readFile "inputs/1"
-  print $ MyLib.getCalibrationValues $ lines dt
+  print $ sum $ MyLib.getCalibrationValues $ lines dt
   
