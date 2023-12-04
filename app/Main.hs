9165fef427f13327a6fa3cfb3a59bd52cdd0b501
Alex <alexeusgr@gmail.com>
2023-12-04 14:48:06 +0200

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,11 @@ module Main where
 --import qualified MyLib (someFunc)
 import Data.Char
 
-splitLines :: [Char] -> [String]
-splitLines dt = 
-  let
-    contents = lines dt
-  in
-    contents
-
 getDigitsInEntries :: [[Char]] -> [[Char]]
 getDigitsInEntries = map (filter isDigit)
-  
 
 main :: IO ()
 main = do
   dt <- readFile "inputs/1"
-  print $ getDigitsInEntries $ splitLines dt
+  print $ getDigitsInEntries $ lines dt
   
