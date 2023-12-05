f7e6143c36942055af3e21a56d746d45299e1c0d
Alex <alexeusgr@gmail.com>
2023-12-05 09:15:14 +0200

    add test values, words and findString

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,6 +1,7 @@
 module MyLib (getCalibrationValues) where
 
 import Data.Char
+import Data.List
 
 getDigitsInEntries :: [[Char]] -> [[Char]]
 getDigitsInEntries = map (filter isDigit)
@@ -14,4 +15,18 @@ intList = map (read::String->Int)
 getCalibrationValues :: [[Char]] -> [Int]
 getCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries xs
 
+myWords :: [String]
+myWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"]
+
+testVals2 :: [String]
+testVals2 = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four",
+             "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+
+findString :: (Eq a) => [a] -> [a] -> Maybe Int
+findString search str = findIndex (isPrefixOf search) (tails str)
+
+-- tests with:
+-- findString (myWords !! 1) (head testVals2)
+
 -- getCorrectedCalibrationValues :: [[Char]] -> [Int]
+-- getCorrectedCalibrationValues  = findString and replaceWordWithDigit
