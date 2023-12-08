deb0071a15960770b2287dcf6427c5e153c217c9
Alex <alexeusgr@gmail.com>
2023-12-08 17:56:38 +0200

    think

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -16,40 +16,68 @@ intList = map (read::String->Int)
 getCalibrationValues :: [[Char]] -> [Int]
 getCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries xs
 
-testVals2 = ["two1nine" , "eightwothree" , "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
-
-dt = head testVals2
 
 data Digits = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving (Show, Enum)
+-- TODO:
+-- dispDigits :: Digits -> Int
+-- dispDigits x = toLower $ show x
+-- this is cool, so what I misunderstand is list vs data, what is the relationship?
 
-findString :: (Eq a) => [a] -> [a] -> Maybe Int
-findString search str = findIndex (isPrefixOf search) (tails str)
+-- findString :: (Eq a) => [a] -> [a] -> Maybe Int
+-- findString search str = findIndex (isPrefixOf search) (tails str)
 
--- :: "two1nine" -> Digit -> "21nine"
-replace :: (Show p, Enum p) => p -> [Char] -> [Char]
-replace n str  =
-  let
-  digit = show (fromEnum n)
-  word = map toLower (show n)
-  pos = findString word str 
-  result = if isJust pos then 
-    take (fromJust pos) str ++ digit ++ drop (fromJust pos + length word) str
-        else str 
-  in  result
+testVals2 = ["two1nine" , "eightwothree" , "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+digs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-replaceAll :: [Char] -> [Char]
-replaceAll str =
-  let
-  replaceZero = replace Zero str
-  replaceOne = replace One replaceZero
-  replaceTwo = replace Two replaceOne
-  replaceThree = replace Three replaceTwo
-  replaceFour = replace Four replaceThree
-  replaceFive = replace Five replaceFour
-  replaceSix = replace Six replaceFive
-  replaceSeven = replace Seven replaceSix
-  replaceEight = replace Eight replaceSeven
-  in replace Nine replaceEight
+dt = testVals2 !! 1
+dig = digs !! 1
 
-getCorrectedCalibrationValues :: [[Char]] -> [Int]
-getCorrectedCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries (map replaceAll xs)
+-- isSubsequenceOf does not work because it returns finds two before eight in eighttwothree 
+
+
+--
+-- :: "t" -> "z" -> False
+-- :: "t" -> "o" -> False
+-- tv3
+-- :: "a" -> "z" -> False
+-- :: "a" -> "o" -> False
+--
+-- :: "a" -> "t" -> False
+--
+
+
+-- bug:
+-- test 1
+-- :: "z" -> "t" -> False
+-- :: "o" -> "t" -> False
+-- :: "t" -> "t" -> True
+
+-- test 2
+-- :: "z" -> "e" -> False
+-- :: "o" -> "e" -> False
+-- :: "t" -> "e" -> True
+-- so the input type is wrong, 
+
+
+
+-- replaceAll :: [Char] -> [Char]
+-- replaceAll str =
+--   let
+--     -- look at position 0
+--     -- compareWith (0, Zero) (0, toLower $ show One) ..
+--     -- if the name of the number 
+--     -- then replace
+--     -- else 
+--     -- recurse on tail
+--     --
+--     -- at top level, how would I describe this operation?
+--     -- I need to find numbers written as words and replace them with digit symbols
+-- 
+--     "eightwothree"
+-- 
+-- 
+--   in str
+
+-- getCorrectedCalibrationValues :: [[Char]] -> [Int]
+getCorrectedCalibrationValues x = x
+-- getCorrectedCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries (map replaceAll xs)
