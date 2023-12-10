67c5b4f5c5d7b71f1017b06fa1fea3ece133e58d
Alex <alexeusgr@gmail.com>
2023-12-10 09:54:31 +0200

    integrate parser into main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import qualified MyLib (getCalibrationValues)
+import qualified MyLib (getCorrectedCalibrationValues)
 
 main :: IO ()
 main = do
   dt <- readFile "inputs/1"
-  print $ sum $ MyLib.getCalibrationValues $ lines dt
+  print $ sum $ MyLib.getCorrectedCalibrationValues $ lines dt
   
