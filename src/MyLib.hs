6767ae6662fd759a7b0b521a1c1ef3e3c28d16a4
Alex <alexeusgr@gmail.com>
2023-12-10 08:16:59 +0200

    refactor and clean, think

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,4 @@
-module MyLib (getCalibrationValues, Digits(..), getCorrectedCalibrationValues, findAndReplaceFirstOccurrence) where
+module MyLib (getCalibrationValues, Digits (..), getCorrectedCalibrationValues, findAndReplaceFirstOccurrence) where
 
 import Data.Char
 import Data.List
@@ -11,27 +11,27 @@ convertToCalibrationValues :: [Char] -> [Char]
 convertToCalibrationValues xs = head xs : [last xs]
 
 intList :: [String] -> [Int]
-intList = map (read::String->Int)
+intList = map (read :: String -> Int)
 
 getCalibrationValues :: [[Char]] -> [Int]
 getCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries xs
 
-
 data Digits = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving (Show, Enum)
 
 wordToDigit :: [Char] -> [Char]
-wordToDigit x = case x of 
-  "one"   -> "1"
-  "two"   -> "2"
-  "three" -> "3"
-  "four"  -> "4"
-  "five"  -> "5"
-  "six"   -> "6"
-  "seven" -> "7"
-  "eight" -> "8"
-  "nine"  -> "9"
-  "zero"  -> "0"
-  _      -> "err"
+wordToDigit x = case x of
+    "one" -> "1"
+    "two" -> "2"
+    "three" -> "3"
+    "four" -> "4"
+    "five" -> "5"
+    "six" -> "6"
+    "seven" -> "7"
+    "eight" -> "8"
+    "nine" -> "9"
+    "zero" -> "0"
+    _ -> "err"
+
 -- TODO:
 -- dispDigits :: Digits -> Int
 -- dispDigits x = toLower $ show x
@@ -40,27 +40,36 @@ wordToDigit x = case x of
 -- findString :: (Eq a) => [a] -> [a] -> Maybe Int
 -- findString search str = findIndex (isPrefixOf search) (tails str)
 
-testVals2 = ["two1nine" , "eightwothree" , "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+testVals2 = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
 digs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 tv = testVals2 !! 1
 dig = digs !! 8
 
 -- this function parses once, but now I need to rerun it until I can say there are no words left in the string.
-findAndReplaceFirstOccurrence :: [Char] -> [String] -> [Char]
-findAndReplaceFirstOccurrence tv [] = head tv : findAndReplaceFirstOccurrence (tail tv) digs 
-findAndReplaceFirstOccurrence tv digs = let
-  dig = head digs
-  l = length dig
-  in
-  if dig == take l tv then wordToDigit dig ++ drop l tv 
-  else findAndReplaceFirstOccurrence tv (tail digs)
+findAndReplaceNumber :: [Char] -> [String] -> [Char]
+findAndReplaceNumber tv [] = head tv : findAndReplaceFirstOccurrence (tail tv) digs
+findAndReplaceNumber tv digs =
+    let dig = head digs
+        l = length dig
+     in if dig == take l tv
+            then wordToDigit dig ++ drop l tv
+            else findAndReplaceFirstOccurrence tv (tail digs)
 
--- findAndReplaceAll tv = let
---   digs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
---   if length tv > 2 then 
+getDigitsInWord tv = map (`isSubsequenceOf` tv) digs
 
+findAndReplaceAll word =
+    let digitList = getDigitsInWord word
+     in if any True digitList
+            then findAndReplaceNumber word digs
+            else word
+
+--   if length tv > 2 then
+--   if string contains no words return string
+--   else find and replace.
+--   map isSubsequenceOf
 
 -- getCorrectedCalibrationValues :: [[Char]] -> [Int]
 getCorrectedCalibrationValues x = x
+
 -- getCorrectedCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries (map replaceAll xs)
