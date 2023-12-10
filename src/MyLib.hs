92b25c37d3aa71ce4b79071c61fdec9448821d45
Alex <alexeusgr@gmail.com>
2023-12-10 06:46:32 +0200

    fix the error

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -47,70 +47,18 @@ tv = testVals2 !! 1
 dig = digs !! 8
 
 function :: [Char] -> [String] -> [Char]
-function tv [] = function (tail tv) digs 
+function tv [] = head tv : function (tail tv) digs 
 function tv digs = let
   dig = head digs
   l = length dig
   in
   if dig == take l tv then replaceWordWithDigit dig ++ drop l tv 
-  else head tv : function tv (tail digs)
+  else function tv (tail digs)
 
--- isSubsequenceOf does not work because it finds two before eight in eightwothree 
--- my function must be more simple
-
--- compare str dn = 
--- compare "eightwothree" "eigth" = "8wothree"
--- if the number word runs out means it is match, we must add tail to the digit name elsewhere
--- myCompare str [] = tail str 
--- if 
--- myCompare str dn =  (head str == head dn) && myCompare tail str tail dn 
-
--- if head str == head dName then compare tail str tail dName
--- else compare str to nextdName
-
+-- 8wothree
 --
--- :: "t" -> "z" -> False
--- :: "t" -> "o" -> False
--- tv3
--- :: "a" -> "z" -> False
--- :: "a" -> "o" -> False
 --
--- :: "a" -> "t" -> False
 --
-
-
--- bug:
--- test 1
--- :: "z" -> "t" -> False
--- :: "o" -> "t" -> False
--- :: "t" -> "t" -> True
-
--- test 2
--- :: "z" -> "e" -> False
--- :: "o" -> "e" -> False
--- :: "t" -> "e" -> True
--- so the input type is wrong, 
-
-
-
--- replaceAll :: [Char] -> [Char]
--- replaceAll str =
---   let
---     -- look at position 0
---     -- compareWith (0, Zero) (0, toLower $ show One) ..
---     -- if the name of the number 
---     -- then replace
---     -- else 
---     -- recurse on tail
---     --
---     -- at top level, how would I describe this operation?
---     -- I need to find numbers written as words and replace them with digit symbols
--- 
---     "eightwothree"
--- 
--- 
---   in str
-
 -- getCorrectedCalibrationValues :: [[Char]] -> [Int]
 getCorrectedCalibrationValues x = x
 -- getCorrectedCalibrationValues xs = intList $ map convertToCalibrationValues $ getDigitsInEntries (map replaceAll xs)
