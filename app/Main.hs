b7788883556cf7f96a3dac59c620db1f73212fa0
Alex <alexeusgr@gmail.com>
2023-12-16 07:46:27 +0200

    clean app

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,5 @@ import qualified MyLib (getCorrectedCalibrationValues)
 main :: IO ()
 main = do
   dt <- readFile "inputs/1"
-  -- print $ lines dt
-  -- print $ MyLib.getCorrectedCalibrationValues $ lines dt
   print $ sum $ MyLib.getCorrectedCalibrationValues $ lines dt
   
